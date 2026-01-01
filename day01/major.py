389d0e2992459c848a480abb8a1b33845a4efc4e
kunalkoli203 <kolikunal336@gmail.com>
2026-01-01 22:11:39 +0530

    day 1

---
+++ day01/major.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+data = {
+    "Marks": [35, 45, 60, 70, 80, 90, 30, 55],
+    "Attendance": [60, 65, 75, 80, 85, 90, 50, 70],
+    "Result": [0, 0, 1, 1, 1, 1, 0, 1]   # 0 = Fail, 1 = Pass
+}
+
+df = pd.DataFrame(data)
+X = df[["Marks", "Attendance"]]
+y = df["Result"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=1
+)
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+new_student = [[65, 75]]   
+result = model.predict(new_student)
+
+if result[0] == 1:
+    print("Prediction: PASS")
+else:
+    print("Prediction: FAIL")
---
+++ day01/minor.py
@@ -0,0 +1,9 @@
+marks = list(map(int, input("Enter marks separated by space: ").split()))
+
+avg = sum(marks) / len(marks)
+highest = max(marks)
+lowest = min(marks)
+
+print("Average Marks:", avg)
+print("Highest Marks:", highest)
+print("Lowest Marks:", lowest)
