0eea5551279ce8ca12bc19e2c7e9126701d6a6cc
kunalkoli203 <kolikunal336@gmail.com>
2026-01-24 18:28:50 +0530

    Update first.py

--- first.py
+++ first.py
@@ -4,3 +4,9 @@ print("hello guys my name is kunal")
 
 print("hello guys my name is kunal")
 print("hello guys my name is kunal")
+print("hello guys my name is kunal")
+print("hello guys my name is kunal")
+
+
+print("hello guys my name is kunal")
+print("hello guys my name is kunal")
